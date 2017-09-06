c41974c98a3af9367b38b9f5b381bce1ec09bfbf
chenyulong <jadedragon.chen@hotmail.com>
2017-09-06 20:02:40 +0800

    使用Verticle部署web项目

--- src/main/java/name/yulong/web/MainVerticle.kt
+++ src/main/java/name/yulong/web/MainVerticle.kt
@@ -1,18 +1,46 @@
 package name.yulong.web
 
-import io.vertx.core.Vertx
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+import io.vertx.core.Handler
+import io.vertx.core.http.HttpServerResponse
+import io.vertx.core.json.Json
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 
-fun main(args: Array<String>) {
+@Suppress("unused")
+class MainVerticle : AbstractVerticle() {
 
-    var server=Vertx.vertx().createHttpServer()
-    server.requestHandler({ request ->
-        request.response().end("<h1>hello world")
-    })
-    server.listen(8080, "127.0.0.1", { res ->
-        if (res.succeeded()) {
-            println("Server is now listening!")
-        } else {
-            println("Failed to bind!")
-        }
-    })
+    override fun start(startFuture: Future<Void>?) {
+        val router = createRouter()
+
+        vertx.createHttpServer()
+                .requestHandler { router.accept(it) }
+                .listen(config().getInteger("http.port", 8080)) { result ->
+                    if (result.succeeded()) {
+                        startFuture?.complete()
+                    } else {
+                        startFuture?.fail(result.cause())
+                    }
+                }
+    }
+
+    private fun createRouter() = Router.router(vertx).apply {
+        get("/").handler(handlerRoot)
+    }
+
+    //
+    // Handlers
+
+    val handlerRoot = Handler<RoutingContext> { req ->
+        req.response().end("Welcome!")
+    }
+
+
+    /**
+     * Extension to the HTTP response to output JSON objects.
+     */
+    fun HttpServerResponse.endWithJson(obj: Any) {
+        this.putHeader("Content-Type", "application/json; charset=utf-8").end(Json.encodePrettily(obj))
+    }
 }
